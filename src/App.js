9e35400d0a7f031e65456cd7b9c66dfa5633aea3
mynamemj <dlaudwp1313@gmail.com>
2019-04-15 20:48:35 +0900

    #3 CSS Modules and SCSS with Parcel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './styles.scss'
 const App =() =>{
     return(
-    <div>lalala</div>
+    <div className={styles.title}>lalala</div>
     );
 }
 export default App;
